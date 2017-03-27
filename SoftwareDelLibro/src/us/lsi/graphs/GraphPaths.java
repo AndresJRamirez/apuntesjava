a8e43343999e5104212dc910954822146a4fc222
AndresJRamirez <donyomismo@gmail.com>
2017-03-27 07:59:43 +0200

    Update GraphPaths.java
    Corrección de nombre de la variable para que haga la concatenación de los dos graphpaths de manera correcta.
    E.g., si r1 es [A->B] y r2 es [B->C], la versión sin corregir devovlería [A->B], la corregida devolvería [A->C] que es lo correcto.

--- SoftwareDelLibro/src/us/lsi/graphs/GraphPaths.java
+++ SoftwareDelLibro/src/us/lsi/graphs/GraphPaths.java
@@ -12,16 +12,16 @@ import com.google.common.collect.Lists;
 /**
  * @author Miguel Toro
  * 
- * <a> Clase de factoría para el tipo GraphPath&lt;V,E&gt; </a>
+ * <a> Clase de factorÃ­a para el tipo GraphPath&lt;V,E&gt; </a>
  *
  */
 public class GraphPaths {
 
 	public static <V,E> GraphPath<V,E> addGraphPath(GraphPath<V,E> r1, GraphPath<V,E> r2){
-		Preconditions.checkArgument(r1.getEndVertex().equals(r2.getStartVertex()),"El vértice final no es igual al inicial");
+		Preconditions.checkArgument(r1.getEndVertex().equals(r2.getStartVertex()),"El vÃ©rtice final no es igual al inicial");
 		List<E> edgeList = Lists.newArrayList(r1.getEdgeList());
 		edgeList.addAll(r2.getEdgeList());
-		return new GraphPathImpl<V,E>(r1.getGraph(), r1.getStartVertex(), r1.getEndVertex(),
+		return new GraphPathImpl<V,E>(r1.getGraph(), r1.getStartVertex(), r2.getEndVertex(),
 				edgeList, r1.getWeight()+r2.getWeight());
 	}
 
